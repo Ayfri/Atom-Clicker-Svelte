22e64b5b86a791d9507b1b6ae0d6efee95254794
Ayfri <pierre.ayfri@gmail.com>
2024-12-21 02:52:39 +0100

    chore(build): switch to cloudflare adapter.

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,7 +5,18 @@ declare global {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
-		// interface Platform {}
+
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & {
+				default: Cache
+			}
+		}
 	}
 
 	interface Window {
