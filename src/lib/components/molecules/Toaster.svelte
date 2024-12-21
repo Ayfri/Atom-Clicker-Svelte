dd1ea4c378c9280583dc8d8562beb5a23ec646e5
Ayfri <pierre.ayfri@gmail.com>
2024-12-21 22:07:02 +0100

    fix(toaster): Improve design of toaster notifications on mobile.

--- src/lib/components/molecules/Toaster.svelte
+++ src/lib/components/molecules/Toaster.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
-  import { removeToast, toasts } from '$stores/toasts'
-  import { fade } from 'svelte/transition'
-  import { X } from 'lucide-svelte'
+	import {removeToast, toasts} from '$stores/toasts';
+	import {X} from 'lucide-svelte';
+	import {fade} from 'svelte/transition';
 </script>
 
-<div class="fixed bottom-8 right-8 z-50 flex flex-col gap-4">
-  {#each $toasts as toast}
-    <div
-      class="flex w-96 flex-col gap-2 overflow-hidden rounded-xl
-      bg-accent-900/90 p-4 shadow-lg backdrop-blur transition-all duration-300
-      hover:scale-[1.02]"
-      class:type={toast.type}
-      transition:fade={{ duration: 400 }}>
-      <div class="flex items-center justify-between gap-4">
-        <h3 class="text-lg font-bold text-white">{toast.title}</h3>
-        <button
-          class="flex h-8 w-8 items-center justify-center rounded-lg
-          transition-colors hover:bg-white/10 hover:*:stroke-[3]"
-          on:click={() => removeToast(toast.id)}>
-          <X class="transition-all duration-300" size={18} />
-        </button>
-      </div>
-      <p class="text-sm text-white/90">
-        {@html toast.message}
-      </p>
-    </div>
-  {/each}
+<div
+	class="fixed bottom-0 right-0 z-50 flex flex-col gap-4 p-4 sm:bottom-8 sm:right-8 sm:p-0"
+>
+	{#each $toasts as toast}
+		<div
+			class="flex w-full flex-col gap-2 overflow-hidden rounded-xl
+			bg-accent-900/90 p-4 shadow-lg backdrop-blur transition-all duration-300
+			hover:scale-[1.02] sm:w-96"
+			class:type={toast.type}
+			transition:fade={{ duration: 400 }}
+		>
+			<div class="flex items-center justify-between gap-4">
+				<h3 class="text-lg font-bold text-white">{toast.title}</h3>
+				<button
+					class="flex h-8 w-8 items-center justify-center rounded-lg transition-colors hover:bg-white/10 hover:*:stroke-[3]"
+					on:click={() => removeToast(toast.id)}
+				>
+					<X class="transition-all duration-300" size={18}/>
+				</button>
+			</div>
+			<p class="text-sm text-white/90">
+				{@html toast.message}
+			</p>
+		</div>
+	{/each}
 </div>
