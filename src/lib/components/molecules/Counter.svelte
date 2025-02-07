5934b87b7f73a3bde7212a5d1e9bf8c93c4e27a6
Ayfri <pierre.ayfri@gmail.com>
2025-02-07 18:30:57 +0100

    refactor(counter): Update Counter component with new styling and protons display.

--- src/lib/components/molecules/Counter.svelte
+++ src/lib/components/molecules/Counter.svelte
@@ -1,76 +1,20 @@
 <script lang="ts">
-	import {atomsPerSecond, atoms, hasBonus} from '$stores/gameStore';
+	import {atomsPerSecond, atoms, hasBonus, protons} from '$stores/gameStore';
 	import {formatNumber} from '$lib/utils';
 </script>
 
-<div class="counter" class:bonus={$hasBonus}>
-	<div class="atoms">
-		<span class="value">{formatNumber($atoms)}</span>
-		<span class="label">atoms</span>
+<div class="mb-8 text-center z-[1] sm:mb-4">
+	<div class="mb-2">
+		{#if $protons > 0}
+			<div>
+				<span id="protons-value" class="text-2xl font-bold text-yellow-400">{formatNumber($protons)}</span>
+				<span class="font-bold text-lg opacity-80">protons</span>
+			</div>
+		{/if}
+		<span id="atoms-value" class=" sm:text-[2.75rem] sm:leading-[1.35] text-5xl font-bold text-accent-500 transition-[filter] duration-200 {$hasBonus ? 'drop-shadow-[0_0_10px_#4a90e2]' : ''}">{formatNumber($atoms)}</span>
+		<span class="font-bold text-2xl opacity-80">atoms</span>
 	</div>
-	<div class="rate">
-		<span class="rate-value">{formatNumber($atomsPerSecond)}</span> atoms per second
+	<div class="text-lg opacity-80 {$hasBonus ? 'opacity-100' : ''}">
+		<span id="atoms-per-second-value" class="font-bold transition-[filter] duration-200 {$hasBonus ? 'drop-shadow-[0_0_7px_currentColor]' : ''}">{formatNumber($atomsPerSecond)}</span> atoms per second
 	</div>
 </div>
-
-<style>
-	.counter {
-		margin-bottom: 2rem;
-		text-align: center;
-		z-index: 1;
-
-		@media screen and (width <= 900px) {
-			margin-bottom: 1rem;
-
-			& .atoms {
-				font-size: 2.75rem;
-				line-height: 1.35;
-				margin-bottom: 0;
-			}
-
-			& .rate {
-				font-size: 1rem;
-			}
-		}
-	}
-
-	.atoms {
-		font-size: 3rem;
-		margin-bottom: 0.5rem;
-	}
-
-	.value {
-		color: #4a90e2;
-		font-weight: bold;
-		transition: filter 0.2s;
-	}
-
-	.label {
-		font-size: 1.5rem;
-		opacity: 0.8;
-	}
-
-	.rate {
-		font-size: 1.2rem;
-		opacity: 0.8;
-	}
-
-	.rate-value {
-		font-weight: bold;
-		transition: filter 0.2s;
-	}
-
-	.bonus {
-		& .value {
-			filter: drop-shadow(0 0 10px currentColor);
-		}
-
-		& .rate {
-			opacity: 1;
-		}
-
-		& .rate-value {
-			filter: drop-shadow(0 0 7px currentColor);
-		}
-	}
-</style>
